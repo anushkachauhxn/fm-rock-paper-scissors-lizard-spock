54ef51724650fc9278ef5368a3f8937279b28072
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-01-30 23:49:54 +0530

    Rules Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Rules from './components/Rules';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Rules />
     </div>
   );
 }
